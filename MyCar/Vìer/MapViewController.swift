8523332343b6f99ef200e84fb8f96342b0887bce
Vlad <evilspirt95@gmail.com>
2021-10-28 22:41:41 +0300

    Make road on Map

--- MyCar/Vìer/MapViewController.swift
+++ MyCar/Vìer/MapViewController.swift
@@ -7,26 +7,91 @@
 
 import UIKit
 import MapKit
-class MapViewController: UIViewController {
-
-   // override func viewDidLoad() {
-    //    super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-   // }
+class MapViewController: UIViewController, MKMapViewDelegate {
+   
+    @IBOutlet weak var MKMapView: MKMapView!
     
-    //title = " work2 "
+   override func viewDidLoad() {
+       super.viewDidLoad()
+       
+       self.MKMapView.delegate = self
+       
+       let sourcePlacemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 55.752578 , longitude: 37.623104))
+       let destinationPlacemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 55.756776 , longitude: 37.594700))
+
+       let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+       let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+
+       let sourceAnnotation = MKPointAnnotation()
+
+       if let location = sourcePlacemark.location {
+           sourceAnnotation.coordinate = location.coordinate
+       }
+       let destinationAnnotation = MKPointAnnotation()
+       if let location = destinationPlacemark.location {
+           destinationAnnotation.coordinate = location.coordinate
+       }
+       self.MKMapView.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
+       
+   let directionRequest = MKDirections.Request()
+       directionRequest.source = sourceMapItem
+       directionRequest.destination = destinationMapItem
+       directionRequest.transportType = .automobile
+
+       // Calculate the direction
+       let directions = MKDirections(request: directionRequest)
+
+       directions.calculate {
+           (response, error) -> Void in
+
+           guard let response = response else {
+               if let error = error {
+                   print("Error: \(error)")
+               }
+               print("Error: 2")
+               return
+           }
+
+           let route = response.routes[0]
+           self.MKMapView.addOverlay(route.polyline, level: .aboveRoads)
+           let rect = route.polyline.boundingMapRect
+           self.MKMapView.setRegion(MKCoordinateRegion(rect), animated: true)
+       }
+   }
+    
+
+   
+    func mapView(_ mapView: MKMapView, rendererFor
+                   overlay: MKOverlay) -> MKOverlayRenderer {
+
+               let renderer = MKPolylineRenderer(overlay: overlay)
+
+               renderer.strokeColor = UIColor(red: 17.0/255.0, green: 147.0/255.0, blue: 255.0/255.0, alpha: 1)
+
+               renderer.lineWidth = 5.0
+
+               return renderer
+           }
+       
+        
+   
 
     var adress: String = ""
     var alertTextField: UITextField?
     
+    @IBAction func AddRoad(_ sender: UIButton) {
+        print("ButtonPrestd")
+       
+        
+        
+    }
     @IBAction func AddAdress(_ sender: UIButton) {
         AlertWithTextFieldAnd2Button(titleAlert: "Add new point", messageAlert: "Write adress of new point", titleButton: "Add")
     }
     @IBAction func AddPoint(_ sender: UIButton) {
         addNewPointOnMap (adress: self.adress)
     }
-    @IBOutlet weak var MKMapView: MKMapView!
+
     
 
     func AlertWithTextFieldAnd2Button(titleAlert: String, messageAlert: String, titleButton: String){
@@ -44,6 +109,8 @@ class MapViewController: UIViewController {
         }))
         self.present(alertView, animated: true, completion: nil)
     }
+    
+    
     func addNewPointOnMap(adress: String){
         let searchRequest = MKLocalSearch.Request()
         searchRequest.naturalLanguageQuery = adress
@@ -69,4 +136,7 @@ class MapViewController: UIViewController {
             }
         }
     }
+    // MARK: - showRouteOnMap
+    // MARK: - MKMapViewDelegate
+   
 }
